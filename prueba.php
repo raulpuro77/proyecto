78cccf950526fb1c4dc4186ae7c7ea8d0d89f213
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-01-18 09:48:49 +0100

    Revert "prueba 2000"
    This reverts commit ccab6dec838d0ef24a112fd04935795ffa5df32a.

--- prueba.php
+++ prueba.php
@@ -12,7 +12,7 @@ for($cuenta=0;$cuenta<10;$cuenta++)
         }
     }
     if($primo){
-        echo "Hola soy Daniel ".$num."<br/>";
+        echo $num."<br/>"."PRIMO";
     } else
     {
         $cuenta--;
